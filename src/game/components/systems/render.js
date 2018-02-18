6dd5a03d8431534715623eb3fbc3aac496cf27e4
yintothayang <yintothayang@gmail.com>
2018-02-17 23:51:30 -0800

    starting to add back systems

---
+++ src/game/components/systems/render.js
@@ -0,0 +1,27 @@
+import {BaseSystem} from 'ein'
+
+export default class RenderSystem extends BaseSystem {
+  constructor(active = true, priority = -1, ctx){
+    super(active, priority);
+    this.ctx = ctx;
+    this.entityProps = ['draw'];
+  }
+
+  //Overide
+  onEntityAdded(manager){
+    this.entities = manager.getEntitiesByProps(this.entityProps);
+  }
+
+  onEntityRemoved(manager){
+    this.entities = manager.getEntitiesByProps(this.entityProps);
+  }
+
+  //Overide
+  update(delta){
+    this.entities.forEach(entity => {
+      this.ctx.save();
+      entity.draw(this.ctx);
+      this.ctx.restore();
+    });
+  }
+};
--- src/game/screens/kingScreen.js
+++ src/game/screens/kingScreen.js
@@ -1,20 +1,51 @@
 import {Screen, Camera} from 'ludic'
-import {Box2D, DebugDraw, World} from 'ludic-box2d'
+import {DebugDraw, World} from 'ludic-box2d'
+import {Engine, BaseSystem} from 'ein'
 import Block from '$entities/block'
+import RenderSystem from '$systems/render'
 
 export default class KingScreen extends Screen {
   constructor(){
     super()
-
   }
 
   onAddedToManager(manager){
     this.initWorld()
-    // console.log(this.$app)
+    this.initSystems()
+  }
+
+  initWorld(){
+    this.camera = new Camera(this.$app.$canvas)
+    this.camera.centerWorldToCamera()
+
+    this.world = new World(0,-9.8)
+    this.debugDraw = DebugDraw.newDebugger(this.$app.$canvas)
+    // this.debugDraw.SetFlags(DebugDraw.e_shapeBit)
+    // this.world.SetDebugDraw(this.debugDraw)
+
+    this.engine = new Engine()
+
+    this.platform = new Block(8,-8,7,1,'blue',this.world, true, -1, false)
   }
 
   initSystems(){
 
+    // Clear
+    this.clearSystem = new BaseSystem(true, -100, (delta)=>{
+      this.$app.$canvas.clear()
+    })
+    this.engine.addSystem(this.clearSystem)
+
+    // Camera
+    this.cameraSystem = new BaseSystem(true, 25, (delta)=>{
+      this.camera.draw(this.$app.$context)
+      this.camera.drawAxes(this.$app.$context)
+    })
+    this.engine.addSystem(this.cameraSystem)
+
+    // Render
+    // this.renderSystem = new RenderSystem(true, 30, this.$app.$context)
+    // this.engine.addSystem(this.renderSystem)
 
   }
 
@@ -22,30 +53,17 @@ export default class KingScreen extends Screen {
 
   }
 
-  initWorld(){
-    console.log("world creator")
-    console.log(Box2D)
-    // this.camera = new Camera(this.$app.$canvas)
-    this.world = new World(0,-9.8)
-    // this.debugDraw = DebugDraw.newDebugger(this.$app.$canvas)
-    // this.debugDraw.SetFlags(DebugDraw.e_shapeBit)
-    // this.world.SetDebugDraw(this.debugDraw)
-
-    // this.platform = new Block(8,-8,7,1,'orange',this.world, true, -1, false)
-  }
-
-
   onDestroy(){
     console.log('onDestroy - KingScreen')
-    // this.app.$ui.$children = []
+    // this.$app.$ui.$children = []
   }
 
   update(delta, time){
     let ctx = this.$app.$context
-    this.$app.$canvas.clear('black')
-    // this.world.step(delta)
-    // this.camera.draw(ctx)
-    // this.camera.drawAxes(ctx)
+    // this.$app.$canvas.clear('black')
+    this.world.step(delta)
+
+    this.engine.update(delta, time)
 
     // draw our box
     // this.boxes.forEach((box)=>{
@@ -54,9 +72,9 @@ export default class KingScreen extends Screen {
     //   ctx.restore()
     // })
 
-    // ctx.save()
-    // this.platform.draw(ctx)
-    // ctx.restore()
-    // this.world.drawDebug(true)
+    ctx.save()
+    this.platform.draw(ctx)
+    ctx.restore()
+    this.world.drawDebug(true)
   }
 }
